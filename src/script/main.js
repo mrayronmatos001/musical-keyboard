0985430d60ec85fcdc15398d93cbfa8e4a935cb0
Ayron Matos <74272557+mrayronmatos001@users.noreply.github.com>
2023-10-17 15:22:05 -0300

    js done

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,56 @@
+const audios = document.querySelectorAll('audio')
 const keys = document.querySelectorAll('.key')
+const audiolist = {}
+const keylist = {}
 
-const renameSet = {
+const functions = {
+    playNote: e => {
+        const audioKeyCode = functions.subfunctions.getKeyCode(e)
+        const key = keylist[audioKeyCode]
 
-    subfunctions: { }
+        if(!key) {
+            return;
+        }
+
+        functions.subfunctions.addPlayingClass(key).playAudio(audioKeyCode)
+    },
+    removePlayNote: e => {
+        e.target.classList.remove("playing")
+    },
+    subfunctions: {
+        getKeyCode: (e) => {
+            return e.type === "keydown" ? e.keyCode : e.target.dataset.key
+        },
+        addPlayingClass: function(key) {
+            key.classList.add('playing')
+            return this
+        },
+        playAudio: function(audioKeyCode) {
+            audiolist[audioKeyCode].currentTime = 0
+            audiolist[audioKeyCode].play()
+            return this
+        }
+    }
 }
 
-const storeObjectsSet = () => {
-    
+const setStoreObjects = () => {
+    keys.forEach(key => keylist[key.dataset.key] = key)
+    audios.forEach(audio => audiolist[audio.dataset.key] = audio)
+
 }
 
 const setListeners = () => {
+    keys.forEach(key => {
+        key.onclick = functions.playNote
+        key.ontransitionend = functions.removePlayNote
+    })
 
+    window.onkeydown = functions.playNote
 }
 
-setListeners()
+const init = () => {
+    setStoreObjects()
+    setListeners()
+}
+
+init()
