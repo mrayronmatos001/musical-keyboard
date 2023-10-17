3e5f94e31cd66ead4ff21390c3dab29edb7b5fc9
Ayron Matos <74272557+mrayronmatos001@users.noreply.github.com>
2023-10-17 04:53:04 -0300

    implemented CSS

---
+++ src/script/main.js
@@ -0,0 +1,16 @@
+const keys = document.querySelectorAll('.key')
+
+const renameSet = {
+
+    subfunctions: { }
+}
+
+const storeObjectsSet = () => {
+    
+}
+
+const setListeners = () => {
+
+}
+
+setListeners()
